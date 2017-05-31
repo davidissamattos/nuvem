771d985a002ddcaaca60bf2441e5784270551144
David Mattos <davidis@chalmers.se>
2017-05-31 11:01:38 +0200

    nginx conf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp /home/pi/nuvem.conf /etc/nginx/sites-available/nuvem.conf
+sudo cp /home/pi/nuvem/nuvem.conf /etc/nginx/sites-available/nuvem.conf
 sudo ln -s /etc/nginx/sites-available/nuvem.conf /etc/nginx/sites-enabled/
 nginx -t
 sudo service nginx reload
