dc3f7980a2a52e654f22f1b4275a7396bffe78a9
David Issa Mattos <issamattos.david@gmail.com>
2017-07-03 16:45:53 +0200

    Writing the first endpoint and functions

--- nuvem.py
+++ nuvem.py
@@ -5,27 +5,11 @@ from sounds.sounds import Sounds
 from lightning.lightning import Lightning
 import pygame
 
-#for keeping the sound on
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
-from sounds.sounds import Keepsoundon
-import atexit
+
 
 app = Flask(__name__)
 #This is to play sounds. It is always initialized and on
 pygame.mixer.init()
-
-# def keep_sound_on():
-#     scheduler = BackgroundScheduler()
-#     scheduler.start()
-#     scheduler.add_job(
-#         func=Keepsoundon,
-#         trigger=IntervalTrigger(seconds=11),
-#         id='sound_on',
-#         name='Keep sound on by pulsing an unaudible beep to the speakers every 60s',
-#         replace_existing=True)
-#     # Shut down the scheduler when exiting the app
-#     atexit.register(lambda: scheduler.shutdown())
 
 @app.route('/')
 def index():
